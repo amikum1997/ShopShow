d0fab89da40175f46c88f4bfe95b76da61fd6359
ams1997 <ams281997@gmail.com>
2020-12-14 02:35:52 +0530

    Project Setup

---
+++ README.md
@@ -0,0 +1,31 @@
+# File Structre
+```
+|   package.json
+|   README.md
+|   
++---app
+|   +---config
+|   +---http
+|   |   +---controllers
+|   |   \---middlewares
+|   \---models
++---public
+|   +---css
+|   |       app.css
+|   |       
+|   +---img
+|   \---js
+|           app.js
+|           
++---resources
+|   +---js
+|   |       app.js
+|   |       
+|   +---scss
+|   |       app.scss
+|   |       
+|   \---views
+\---routes
+        api.js
+        web.js
+```
---
+++ server.js
@@ -0,0 +1,22 @@
+//Import Packages
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const expressLayout = require('express-ejs-layouts');
+const path = require('path');
+const PORT = process.env.PORT || 4000
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+
+// configure template engine
+app.use(expressLayout);
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs');
+
+// Listen Tlo PORT
+app.listen(PORT, () => {
+    console.log(`Listening To Port ${PORT}`)
+})
